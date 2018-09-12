2c6e0b601272b4477ef2875a610aa9e698e21554
XXinn <38495962+XXinn@users.noreply.github.com>
2018-09-12 22:01:35 +0800

    Update Equipment.java

--- src/device/Equipment.java
+++ src/device/Equipment.java
@@ -13,17 +13,12 @@ import memory.hardware.PCB;
  *
  */
 public class Equipment {
-	//每个设备有一个设备分配表和一个等待序列
-	private int numOfDevice;
-	public DMT[] dmts;
-	public Queue<WaitProcess> waitList = new LinkedList<>();
-
 	/*
-	 * A设备2个，B设备3个，C设备3个
+	 * 每个设备有一个设备分配表和一个等待序列
 	 */
-	public static Equipment A =new Equipment(2);
-	public static Equipment B =new Equipment(3);
-	public static Equipment C =new Equipment(3);
+	private int numOfDevice;
+	public DMT[] dmts;//设备分配表
+	public Queue<WaitProcess> waitList = new LinkedList<>();//等待序列
 
 
 	public DMT[] getDmts() {
@@ -39,6 +34,7 @@ public class Equipment {
 	}
 
 	public Equipment(int num){
+		//初始同类设备的数量
 		DMT[] value = new DMT[num];
 		this.dmts = value;
 		this.numOfDevice=num;
