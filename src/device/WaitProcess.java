df52ffe3a0bfeb1c1076cd4f1cfd76c16424ffb0
XXinn <38495962+XXinn@users.noreply.github.com>
2018-09-12 22:02:42 +0800

    Update WaitProcess.java

--- src/device/WaitProcess.java
+++ src/device/WaitProcess.java
@@ -3,6 +3,9 @@ package device;
 import memory.hardware.PCB;
 
 public class WaitProcess {
+	/*
+	 * 等待信息块，以整个放进等待序列
+	 */
 	private PCB pcb;
 	private int time = 0;
 	public PCB getPcb() {
@@ -17,8 +20,9 @@ public class WaitProcess {
 	public void setTime(int time) {
 		this.time = time;
 	}
-	
+
 	public WaitProcess(PCB pcb,int time){
+		//把进程存进等待序列
 		this.pcb = pcb;
 		this.time = time;
 	}
