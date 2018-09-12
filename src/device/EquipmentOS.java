3c67cfb03741cd2757b4d372d5f63c28b27d19ad
XXinn <38495962+XXinn@users.noreply.github.com>
2018-09-12 22:02:15 +0800

    Update EquipmentOS.java

--- src/device/EquipmentOS.java
+++ src/device/EquipmentOS.java
@@ -1,8 +1,14 @@
 package device;
 import memory.hardware.PCB;
 import process.hardware.*;
-public class EquipmentOS {
+import process.software.*;
+import process.software.Process;
+import process.software.impl.Controller;
 
+public class EquipmentOS {
+	/*
+	 * 设备管理
+	 */
 	public void apply(PCB pcb,Equipment num,int time){
 		//申请设备
 		WaitProcess value = new WaitProcess(pcb, time);
@@ -11,6 +17,7 @@ public class EquipmentOS {
 		}else if(num.getNumOfDevice()>0){
 			for(int i=0;i<num.getDmts().length;i++){
 				if(num.dmts[i].getStatus()==0){
+					//有空闲设备就从等待队列移除队首
 					num.dmts[i].setStatus(1);
 					num.setNumOfDevice(num.getNumOfDevice()-1);
 					num.dmts[i].useDevice(pcb, time);
@@ -19,19 +26,23 @@ public class EquipmentOS {
 					 */
 					num.dmts[i].setStatus(0);
 					num.setNumOfDevice(num.getNumOfDevice()+1);
-					finish(num);
+					finish(num,pcb);
 
 				}
 			}
 		}
 	}
 
-	public void finish(Equipment value){
+	public void finish(Equipment value,PCB pcb){
+		//设备运行结束
+		Process mothod = new Process();
+		mothod.awake(pcb);//释放进程
 		if(!value.waitList.isEmpty()){
+			//检查等待序列是否有进程
 			WaitProcess mode = value.waitList.remove();
-			PCB pcb = mode.getPcb();
+			PCB otherpcb = mode.getPcb();
 			int time = mode.getTime();
-			apply(pcb,value,time);
+			apply(otherpcb,value,time);
 		}
 	}
 }
